db51fd7f259652750502114138f879af71997179
shreq <shrek.kresh@o2.pl>
2019-03-16 20:03:02 +0100

    trying gui

--- zad1/src/main/java/Main.java
+++ zad1/src/main/java/Main.java
@@ -1,12 +1,19 @@
 import Charts.Drawer;
 import Signals.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.jfree.chart.ui.UIUtils;
 
 import java.util.Map;
 
-public class Main {
+public class Main extends Application {
 
     public static void main(String[] args) {
+
+        //launch(args);
 
         Signal nud = new NoiseUniformDistribution(1, 0, 50);
         Signal ng = new NoiseGaussian(0, 50);
@@ -26,5 +33,13 @@ public class Main {
         d.pack();
         UIUtils.centerFrameOnScreen(d);
         d.setVisible(true);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        stage.setTitle("Hello World");
+        stage.setScene(new Scene(root, 300, 275));
+        stage.show();
     }
 }
